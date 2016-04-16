6b5fa42a25b2bdf85dfeca3f1455a1da69d30dde
dekuRockShooter <dekuRockShooter@gmail.com>
2016-04-16 06:50:46 -0700

    Add methods for colliding with Bullets and Ships.

--- wingman/game/Ship.java
+++ wingman/game/Ship.java
@@ -9,73 +9,79 @@ import wingman.modifiers.weapons.AbstractWeapon;
 
 /* Ships are things that have weapons and health */
 public class Ship extends MoveableObject {
-	protected AbstractWeapon weapon;
-	protected int health;
-	protected Point gunLocation;
-	
+    protected AbstractWeapon weapon;
+    protected int health;
+    protected Point gunLocation;
+    
     public Ship(Point location, Point speed, int strength, Image img){
-    	super(location,speed,img);
-    	this.strength=strength;
-    	this.health=strength;
-    	this.gunLocation = new Point(15,20);
+        super(location,speed,img);
+        this.strength=strength;
+        this.health=strength;
+        this.gunLocation = new Point(15,20);
     }
     
     public Ship(int x, Point speed, int strength, Image img){
-    	this(new Point(x,-90), speed, strength, img);
+        this(new Point(x,-90), speed, strength, img);
     }
     
     public void setWeapon(AbstractWeapon weapon){
-    	this.weapon.remove();
-    	this.weapon = weapon;
+        this.weapon.remove();
+        this.weapon = weapon;
     }
     
     public AbstractWeapon getWeapon(){
-    	return this.weapon;
+        return this.weapon;
     }
     
     public void damage(int damageDone){
-    	this.health -= damageDone;
-    	if(health<=0){
-    		this.die();
-    	}
-    	return;
+        this.health -= damageDone;
+        if(health<=0){
+            this.die();
+        }
+        return;
     }
     
     public void die(){
-    	this.show=false;
-    	SmallExplosion explosion = new SmallExplosion(new Point(location.x,location.y));
-    	GameWorld.getInstance().addBackground(explosion);
-    	weapon.deleteObserver(this);
-    	motion.deleteObserver(this);
-    	GameWorld.getInstance().removeClockObserver(motion);
+        this.show=false;
+        SmallExplosion explosion = new SmallExplosion(new Point(location.x,location.y));
+        GameWorld.getInstance().addBackground(explosion);
+        weapon.deleteObserver(this);
+        motion.deleteObserver(this);
+        GameWorld.getInstance().removeClockObserver(motion);
     }
     
     public void collide(GameObject otherObject){
     }
+
+    public void collide(Ship otherObject){
+    }
+
+    public void collide(Bullet otherObject){
+    }
     
     public void fire()
     {
-    	weapon.fireWeapon(this);
+        weapon.fireWeapon(this);
     }
     
     /* some setters and getters!*/
     public void setHealth(int health){
-    	this.health = health;
+        this.health = health;
     }
     
     public int getHealth(){
-    	return health;
+        return health;
     }
     
     public MotionController getMotion(){
-    	return this.motion;
+        return this.motion;
     }
     
     public void setMotion(MotionController motion){
-    	this.motion = motion;
+        this.motion = motion;
     }
     
     public Point getGunLocation(){
-    	return this.gunLocation;
+        return this.gunLocation;
     }
 }
