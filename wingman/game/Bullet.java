63f891891918229b0e7412e2a131aa3435ea8edc
dekuRockShooter <dekuRockShooter@gmail.com>
2016-04-16 09:19:57 -0700

    Add constructor for setting the Bullet's image.

--- wingman/game/Bullet.java
+++ wingman/game/Bullet.java
@@ -1,35 +1,42 @@
 package wingman.game;
 
 import java.awt.Point;
+import java.awt.Image;
 
 import wingman.GameWorld;
 import wingman.modifiers.motions.MotionController;
 
 /*Bullets fired by player and enemy weapons*/
 public class Bullet extends MoveableObject {
-	PlayerShip owner;
-	boolean friendly;
-	
-	public Bullet(Point location, Point speed, int strength, MotionController motion, GameObject owner){
-		super(location, speed, GameWorld.sprites.get("enemybullet1"));
-		this.strength=strength;
-		if(owner instanceof PlayerShip){
-			this.owner = (PlayerShip) owner;
-			this.friendly=true;
-			this.setImage(GameWorld.sprites.get("bullet"));
-		}
-		this.motion = motion;
-		motion.addObserver(this);
-	}
-	
-	public PlayerShip getOwner(){
-		return owner;
-	}
-	
-	public boolean isFriendly(){
-		if(friendly){
-			return true;
-		}
-		return false;
-	}
+    PlayerShip owner;
+    boolean friendly;
+    
+    public Bullet(Point location, Point speed, int strength, MotionController motion, GameObject owner){
+        super(location, speed, GameWorld.sprites.get("enemybullet1"));
+        this.strength=strength;
+        if(owner instanceof PlayerShip){
+            this.owner = (PlayerShip) owner;
+            this.friendly=true;
+            this.setImage(GameWorld.sprites.get("bullet"));
+        }
+        this.motion = motion;
+        motion.addObserver(this);
+    }
+
+    public Bullet(Point location, Point speed, int strength,
+                  MotionController motion, GameObject owner, Image img){
+        this(location, speed, strength, motion, owner);
+        this.setImage(img);
+    }
+    
+    public PlayerShip getOwner(){
+        return owner;
+    }
+    
+    public boolean isFriendly(){
+        if(friendly){
+            return true;
+        }
+        return false;
+    }
 }
