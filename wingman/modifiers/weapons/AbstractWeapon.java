90bd0c028dd467cc596cd95c1d150962acf32d1b
dekuRockShooter <dekuRockShooter@gmail.com>
2016-04-23 09:42:17 -0700

    Change indentation.

--- wingman/modifiers/weapons/AbstractWeapon.java
+++ wingman/modifiers/weapons/AbstractWeapon.java
@@ -10,33 +10,33 @@ import wingman.modifiers.AbstractGameModifier;
  * They observe motions and are observed by the Game World
  */
 public abstract class AbstractWeapon extends AbstractGameModifier {
-	Bullet[] bullets;
-	boolean friendly;
-	int lastFired=0, reloadTime;
-	protected int direction;
-	public int reload = 5;
-	
-	public AbstractWeapon(){
-		super();
-		this.addObserver(GameWorld.getInstance());
-	}
-	
-	public void fireWeapon(Ship theShip){
-		if(theShip instanceof PlayerShip){
-			direction = 1;
-		}
-		else{
-			direction = -1;
-		}
-	}
-	
-	/* read is called by Observers when they are notified of a change */
-	public void read(Object theObject) {
-		GameWorld world = (GameWorld) theObject;
-		world.addBullet(bullets);	
-	}
-	
-	public void remove(){
-		this.deleteObserver(GameWorld.getInstance());
-	}
+    Bullet[] bullets;
+    boolean friendly;
+    int lastFired=0, reloadTime;
+    protected int direction;
+    public int reload = 5;
+    
+    public AbstractWeapon(){
+        super();
+        this.addObserver(GameWorld.getInstance());
+    }
+    
+    public void fireWeapon(Ship theShip){
+        if(theShip instanceof PlayerShip){
+            direction = 1;
+        }
+        else{
+            direction = -1;
+        }
+    }
+    
+    /* read is called by Observers when they are notified of a change */
+    public void read(Object theObject) {
+        GameWorld world = (GameWorld) theObject;
+        world.addBullet(bullets);   
+    }
+    
+    public void remove(){
+        this.deleteObserver(GameWorld.getInstance());
+    }
 }
