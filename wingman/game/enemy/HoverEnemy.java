067a1ad3d49b7a6c5bb89ab793db5f00bcee4e5b
dekuRockShooter <dekuRockShooter@gmail.com>
2016-04-26 10:07:55 -0700

    Replace weapon with NullWeapon.

--- wingman/game/enemy/HoverEnemy.java
+++ wingman/game/enemy/HoverEnemy.java
@@ -6,18 +6,19 @@ import wingman.GameWorld;
 import wingman.game.Ship;
 import wingman.modifiers.motions.CentralHorizontalHover;
 import wingman.modifiers.weapons.SpreadWeapon;
+import wingman.modifiers.weapons.NullWeapon;
 
 public class HoverEnemy extends Ship {
-	public HoverEnemy(int location){
-		this(location, new Point(3,3));
-	}
-	
-	public HoverEnemy(int location, Point speed){
-		super(location, speed, 20, GameWorld.sprites.get("enemy3"));
-		strength=10;
-		health=10;
-		this.weapon = new SpreadWeapon();
-		
-		motion = new CentralHorizontalHover();
-	}
+    public HoverEnemy(int location){
+        this(location, new Point(3,3));
+    }
+    
+    public HoverEnemy(int location, Point speed){
+        super(location, speed, 20, GameWorld.sprites.get("enemy3"));
+        strength=10;
+        health=10;
+        this.weapon = new NullWeapon();
+        
+        motion = new CentralHorizontalHover();
+    }
 }
