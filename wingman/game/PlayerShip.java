47e977b254366a1e95b0ff4735bba78d1615884b
dekuRockShooter <dekuRockShooter@gmail.com>
2016-04-29 08:55:49 -0700

    Add method to increment life.

--- wingman/game/PlayerShip.java
+++ wingman/game/PlayerShip.java
@@ -39,104 +39,108 @@ public class PlayerShip extends Ship implements Observer {
     }
 
     public void draw(Graphics g, ImageObserver observer) {
-    	if(respawnCounter<=0)
-    		g.drawImage(img, location.x, location.y, observer);
-    	else if(respawnCounter==80){
-    		GameWorld.getInstance().addClockObserver(this.motion);
-    		respawnCounter -=1;
-    	}
-    	else if(respawnCounter<80){
-    		if(respawnCounter%2==0) g.drawImage(img, location.x, location.y, observer);
-    		respawnCounter -= 1;
-    	}
-    	else
-    		respawnCounter -= 1;
+        if(respawnCounter<=0)
+            g.drawImage(img, location.x, location.y, observer);
+        else if(respawnCounter==80){
+            GameWorld.getInstance().addClockObserver(this.motion);
+            respawnCounter -=1;
+        }
+        else if(respawnCounter<80){
+            if(respawnCounter%2==0) g.drawImage(img, location.x, location.y, observer);
+            respawnCounter -= 1;
+        }
+        else
+            respawnCounter -= 1;
     }
     
     public void damage(int damageDone){
-    	if(respawnCounter<=0)
-    		super.damage(damageDone);
+        if(respawnCounter<=0)
+            super.damage(damageDone);
     }
     
     public void update(int w, int h) {
-    	if(isFiring){
-    		int frame = GameWorld.getInstance().getFrameNumber();
-    		if(frame>=lastFired+weapon.reload){
-    			fire();
-    			lastFired= frame;
-    		}
-    	}
-    	
-    	if((location.x>0 || right==1) && (location.x<w-width || left==1)){
-    		location.x+=(right-left)*speed.x;
-    	}
-    	if((location.y>0 || down==1) && (location.y<h-height || up==1)){
-    		location.y+=(down-up)*speed.x;
-    	}
+        if(isFiring){
+            int frame = GameWorld.getInstance().getFrameNumber();
+            if(frame>=lastFired+weapon.reload){
+                fire();
+                lastFired= frame;
+            }
+        }
+        
+        if((location.x>0 || right==1) && (location.x<w-width || left==1)){
+            location.x+=(right-left)*speed.x;
+        }
+        if((location.y>0 || down==1) && (location.y<h-height || up==1)){
+            location.y+=(down-up)*speed.x;
+        }
     }
     
     public void startFiring(){
-    	isFiring=true;
+        isFiring=true;
     }
     
     public void stopFiring(){
-    	isFiring=false;
+        isFiring=false;
     }
     
     public void fire()
     {
-    	if(respawnCounter<=0){
-    		weapon.fireWeapon(this);
-    		GameWorld.getInstance().sound.play("Resources/snd_explosion1.wav");
-    	}
+        if(respawnCounter<=0){
+            weapon.fireWeapon(this);
+            GameWorld.getInstance().sound.play("Resources/snd_explosion1.wav");
+        }
     }
     
     public void die(){
-    	this.show=false;
-    	BigExplosion explosion = new BigExplosion(new Point(location.x,location.y));
-    	GameWorld.getInstance().addBackground(explosion);
-    	lives-=1;
-    	if(lives>=0){
-        	GameWorld.getInstance().removeClockObserver(this.motion);
-    		reset();
-    	}
-    	else{
-    		this.motion.delete(this);
-    	}
+        this.show=false;
+        BigExplosion explosion = new BigExplosion(new Point(location.x,location.y));
+        GameWorld.getInstance().addBackground(explosion);
+        lives-=1;
+        if(lives>=0){
+            GameWorld.getInstance().removeClockObserver(this.motion);
+            reset();
+        }
+        else{
+            this.motion.delete(this);
+        }
     }
     
     public void reset(){
-    	this.setLocation(resetPoint);
-    	health=strength;
-    	respawnCounter=160;
-    	this.weapon = new SimpleWeapon();
+        this.setLocation(resetPoint);
+        health=strength;
+        respawnCounter=160;
+        this.weapon = new SimpleWeapon();
     }
     
     public int getLives(){
-    	return this.lives;
+        return this.lives;
     }
     
     public int getScore(){
-    	return this.score;
+        return this.score;
     }
     
     public String getName(){
-    	return this.name;
+        return this.name;
     }
     
     public void incrementScore(int increment){
-    	score += increment;
+        score += increment;
     }
     
     public boolean isDead(){
-    	if(lives<0 && health<=0)
-    		return true;
-    	else
-    		return false;
+        if(lives<0 && health<=0)
+            return true;
+        else
+            return false;
+    }
+
+    public void incrementLife() {
+        lives = lives + 1;
     }
     
-	public void update(Observable o, Object arg) {
-		AbstractGameModifier modifier = (AbstractGameModifier) o;
-		modifier.read(this);
-	}
+    public void update(Observable o, Object arg) {
+        AbstractGameModifier modifier = (AbstractGameModifier) o;
+        modifier.read(this);
+    }
 }
